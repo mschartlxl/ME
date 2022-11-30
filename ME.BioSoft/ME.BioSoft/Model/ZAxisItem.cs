5c93dc1d7c75433ffe3dbc625b9b4cc0e656c48a
lxl <lxl_mengquan@163.com>
2022-11-30 15:42:39 +0800

    Z轴添加

---
+++ ME.BioSoft/ME.BioSoft/Model/ZAxisItem.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ME.BioSoft.Model
+{
+    public class ZAxisItem: ObservableObject
+    {
+        public ZAxisItem()
+        {
+
+        }
+        private bool isCheck = true;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsCheck
+        {
+            get => isCheck;
+            set
+            {
+                isCheck = value;
+                SetProperty(ref isCheck, value);
+            }
+        }
+        private string ckContent;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CkContent
+        {
+            get => ckContent;
+            set
+            {
+                ckContent = value;
+                SetProperty(ref ckContent,value);
+            }
+        }
+        
+    }
+}
